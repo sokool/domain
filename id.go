c3ea5d840d8950db06076a4dc1175258e4b14e85
sokool <marian.sokolowski@gmail.com>
2023-03-28 12:23:55 +0200

    domain.ID refactor

--- id.go
+++ id.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	"encoding"
-	"reflect"
 
 	"github.com/google/uuid"
 )
@@ -13,28 +12,27 @@ func (id *ID) IsEmpty() bool {
 	return id.UUID == uuid.Nil
 }
 
-func NewID[T encoding.TextUnmarshaler](s ...string) (t T, err error) {
+func NewID[T encoding.TextUnmarshaler](t T, s ...string) (err error) {
 	var u uuid.UUID
 	var b []byte
 
 	if len(s) == 0 {
 		if u, err = uuid.NewRandom(); err != nil {
-			return t, Errorf("%w", err)
+			return Errorf("%w", err)
 		}
 		if b, err = u.MarshalText(); err != nil {
-			return t, Errorf("%w", err)
+			return Errorf("%w", err)
 		}
 	} else {
 		b = []byte(s[0])
 		if u, err = uuid.ParseBytes(b); err != nil {
-			return t, Errorf("%w", err)
+			return Errorf("%w", err)
 		}
 	}
 
-	t = reflect.New(reflect.TypeOf(t).Elem()).Interface().(T)
 	if err = t.UnmarshalText(b); err != nil {
-		return t, Errorf("%w", err)
+		return Errorf("%w", err)
 	}
 
-	return t, nil
+	return nil
 }
--- id_test.go
+++ id_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestNewID(t *testing.T) {
-
-	if _, err := domain.NewID[*domain.ID]("asd"); err == nil {
+	var id domain.ID
+	if err := domain.NewID(&id, "asd"); err == nil {
 		t.Fatal(err)
 	}
 
-	if _, err := domain.NewID[*domain.ID]("86b7cdbb-1c03-409e-a6e9-65501ff97036"); err != nil {
+	if err := domain.NewID(&id, "86b7cdbb-1c03-409e-a6e9-65501ff97036"); err != nil {
 		t.Fatal(err)
 	}
 }
